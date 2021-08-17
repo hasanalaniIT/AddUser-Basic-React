ea3ab753b09655111213f79b9533af8ee2d31ffa
Hasan <hasanalaniIT@users.noreply.github.com>
2021-08-17 17:37:55 +0300

    Adding Card to wrap the form inside it with its css and Adding costom ui component for Button and its css file

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,18 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import Card from "../UI/Card";
+import classes from './AddUser.module.css'
+import Button from "../UI/Button";
 const AddUser = (props) => {
-    const AddUserHandler= (event) => {
-        event.preventDefualt()
-    }
-    return (
-        <form onSubmit={AddUserHandler}>
-          <label htmlFor="username">Username</label>  
-          <input id="username" type="text" />
-          <label htmlFor="age">Age (Years)</label>  
-          <input id="age" type="number" />
-          <button type="submit">Add User</button>
-        </form>
-    )
-}
+  const AddUserHandler = (event) => {
+    event.preventDefualt();
+  };
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={AddUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
+};
 
-export default AddUser
+export default AddUser;
